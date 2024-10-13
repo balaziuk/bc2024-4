c7bc09c05861bd7b6b4507bed511c06c51a57b8f
balaziuk bc2024-4 <balaztasi@gmail.com>
2024-10-13 22:52:48 +0300

    added method put

--- bc2024-4.js
+++ bc2024-4.js
@@ -21,14 +21,34 @@ const server = http.createServer(async (req, res) => {
     const statusCode = req.url.slice(1); 
     const filePath = getCachedFilePath(statusCode);
 
-    if (req.method === 'GET') {
+    if (req.method === 'PUT') {
+        let body = [];
+    
+        req.on('data', (chunk) => {
+            body.push(chunk);  
+        });
+    
+        req.on('end', async () => {
+            body = Buffer.concat(body);  
+    
+            try {
+                await fs.writeFile(filePath, body);
+                res.writeHead(201, { 'Content-Type': 'text/plain' });
+                res.end('Created');  
+            } catch (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            }
+        });
+    } else if (req.method === 'GET') {
+        // Обробляємо GET-запит для отримання картинки
         try {
-            const image = await fs.readFile(filePath);
-            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-            res.end(image);
+          const image = await fs.readFile(filePath);
+          res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+          res.end(image);
         } catch (err) {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('Not Found');
         }
     } else {
         res.writeHead(405, { 'Content-Type': 'text/plain' });
